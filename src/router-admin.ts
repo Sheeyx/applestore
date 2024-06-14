eed0deaae4534fb024a9429eac502de0277eb076
Sheeyx <sheeyx00@gmail.com>
2024-06-15 03:16:54 +0900

    feat: create product controller & product routing apis

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 const routerAdmin = express.Router();
 import storeController from './controllers/store.controller';
+import productController from './controllers/product.controller';
 
 
 /* Stores */
 routerAdmin.get('/', storeController.goHome);
 
 routerAdmin
-.get('/login', storeController.getLogin)
-.post('/login', storeController.processLogin);
+    .get('/login', storeController.getLogin)
+    .post('/login', storeController.processLogin);
 
 routerAdmin
 .post('/signup', storeController.processSignup);
@@ -16,6 +17,11 @@ routerAdmin.get('/check-me', storeController.checkAuthSession);
 routerAdmin.get('/logout', storeController.logout);
 
 /* Product */
+routerAdmin
+    .get('/product/all', productController.getAllProducts)
+    .post('/product/create', productController.createNewProduct)
+    .post('/product/:id', productController.updateChosenProduct);
+
 /* User */
 
 export default routerAdmin;
