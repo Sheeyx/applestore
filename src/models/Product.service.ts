079e01787e12d87f87c68b44fb523d5e0b256b74
Sheeyx <sheeyx00@gmail.com>
2024-06-29 06:24:48 +0900

    fix: modify getProduct business logic

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -4,49 +4,22 @@ import Errors, { HttpCode, Message } from "../libs/Errors";
 import { ProductInput, Product, ProductUpdateInput, ProductInquire } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 import { T } from "../libs/types/common";
-import { ObjectId } from "mongoose";
+import {ObjectId} from 'mongoose';
+import ViewServiceModel from "./View.service";
+import { ViewInput } from "../libs/types/view";
+import { ViewGroup } from "../libs/enums/view.enum";
 
 class ProductService {
     private readonly productModel;
+    public viewService;
     
     constructor(){
         this.productModel = ProductModel;
+        this.viewService = new ViewServiceModel;
     }
 
 
     //SPA
-    //SSR
-
-    // Create
-    public async createNewProduct(input: ProductInput): Promise<any> {
-        try {
-            return await this.productModel.create(input);          
-        } catch (error) {
-            console.log("Error model:createNewProduct",error);
-            throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED)
-            
-        }
-    }
-
-    // Get All
-    public async getAllProducts(): Promise<any[]> {
-        const result = await this.productModel.find().exec();
-        if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
-
-        return result;
-    }
-
-    //Update
-    public async updateChosenProduct(id: string,input: ProductUpdateInput): Promise<any> {
-        // string = Obeject id
-        id = shapeIntoMongooseObjectId(id);
-        const result = await this.productModel.findByIdAndUpdate({_id: id}, input, { new:true } ).exec();
-        if(!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
-        console.log("result", result);
-
-        return result;
-        
-    }
 
     public async getProducts(inquire: ProductInquire): Promise<Product[]>{
         const match: T = { productStatus: ProductStatus.PROCESS}
@@ -80,9 +53,67 @@ class ProductService {
 
         if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 
+
+        if(memberId){
         // TODO If authenticated users => first => view log creation
+        // Check view log existance
+        const input: ViewInput = {
+            memberId: memberId,
+            viewRefId: productId,
+            viewGroup: ViewGroup.PRODUCT
+        }
+        const existView = await this.viewService.checkViewExistence(input);
+
+        console.log("exist",!!existView);
+        
+        if(!existView) {
+        // Insert New View Log
+            await this.viewService.insertMemberView(input);
+        // Increase Target View 
+            const result2 = await this.productModel
+            .findByIdAndUpdate(
+                productId, 
+                {$inc: {productViews: +1}},
+                {new: true}
+                )
+            
+        }
+
+        }
 
         return result;
+    }
+    //SSR
+
+    // Create
+    public async createNewProduct(input: ProductInput): Promise<any> {
+        try {
+            return await this.productModel.create(input);          
+        } catch (error) {
+            console.log("Error model:createNewProduct",error);
+            throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED)
+            
+        }
+    }
+
+    // Get All
+    public async getAllProducts(): Promise<any> {
+        const result = await this.productModel.find().exec();
+        if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+        return result;
+    }
+
+    //Update
+    public async updateChosenProduct(id: string,input: ProductUpdateInput): Promise<any> {
+        // string = Obeject id
+        id = shapeIntoMongooseObjectId(id);
+        const result = await this.productModel.findByIdAndUpdate({_id: id}, input, { new:true } ).exec();
+        if(!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
+        console.log("result", result);
+
+        return result;
+        
     }
 }
 
